1c6170587260e34ecbcc303b825ce519fc5a05a4
Yu Hua Cheng <ycheng@pulsepoint.com>
2018-03-18 00:29:14 -0400

    cluster - webworker-threads

--- event-loop/index.js
+++ event-loop/index.js
@@ -1,12 +1,31 @@
 const crypto = require('crypto');
 const express = require('express');
+const Worker = require('webworker-threads').Worker;
 
 const app = express();
 
 app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-        res.send('hi there');
+
+    const worker = new Worker(function() {
+        // worker on different thread
+        // anything inside is a different process, which can not use variable outside this scope
+        this.onmessage = function(){
+            let counter = 0;
+            while (counter < 1e9){
+                counter++;
+            }
+
+            postMessage(counter)
+        }
     });
+
+    // application side worker
+    worker.onmessage = function (message) {
+        console.log(message.data);
+        res.send('' + message.data);
+    };
+
+    worker.postMessage();
 });
 
 app.get('/fast', (req, res) => {
